68db8597350125d0a8e607137317788780e7b30e
ternlef11 <aliarazzak@hotmail.com>
2020-06-18 23:45:34 +0200

    upgrade

---
+++ build/precache-manifest.f5f11ccc178b34a85dbca6d4e4e8f35b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7449d381a32ae26230cbb254467c1777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae8163f25a1aafe41a33",
+    "url": "/static/css/main.a71ab294.chunk.css"
+  },
+  {
+    "revision": "0780bb69380bde06856c",
+    "url": "/static/js/2.7b5fce84.chunk.js"
+  },
+  {
+    "revision": "ae8163f25a1aafe41a33",
+    "url": "/static/js/main.9b54b312.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "634e1b4150fb5ce5ffd96deabe59c53f",
+    "url": "/static/media/eggblack_sma.634e1b41.gif"
+  },
+  {
+    "revision": "6bf21933a0b89ca18c32f8a42241954d",
+    "url": "/static/media/eggwhite_sma.6bf21933.gif"
+  }
+]);
